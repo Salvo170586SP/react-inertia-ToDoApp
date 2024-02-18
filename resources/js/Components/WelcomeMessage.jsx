840339b697cc993b07b743cf73b55f43922d48aa
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2024-02-18 13:46:11 +0100

    fix welcome message

--- resources/js/Components/WelcomeMessage.jsx
+++ resources/js/Components/WelcomeMessage.jsx
@@ -1,6 +1,6 @@
 export default function WelcomeMessage({hideWelcomeScreen}) {
     return (
-        <div className="md:h-[400px] md:w-[400px] p-9 mx-5 flex flex-col items-center justify-between shadow-xl rounded-2xl bg-slate-100 border border-slate-300">
+        <div className="md:h-[400px] md:w-[400px] p-9 mx-5 flex flex-col items-center justify-between shadow-xl rounded-2xl bg-slate-100 dark:bg-slate-700 dark:text-white border border-slate-300 dark:border-slate-700">
             <h2 className="text-2xl font-bold">BENVENUTO</h2>
             <p className="text-justify mt-2 italic">
                 Incomincia subito a creare la tua lista cliccando il pulsante
@@ -14,7 +14,7 @@ export default function WelcomeMessage({hideWelcomeScreen}) {
             </p>
 
             <button
-                className="bg-blue-500 text-white px-5 py-2 rounded-xl"
+                className="bg-blue-500 text-white px-5 py-2 rounded-xl dark:bg-gray-900"
                 onClick={hideWelcomeScreen}
             >
                 chiudi questo messaggio
